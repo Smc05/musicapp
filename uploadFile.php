35e6209fb70074256f03c010fb5ee7f8677a8cc4
Smc05 <124348972+Smc05@users.noreply.github.com>
2023-02-21 22:12:13 +0100

    Update uploadFile.php

--- uploadFile.php
+++ uploadFile.php
@@ -1,6 +1,5 @@
 <?php
-$FileName="media/musics/" . basename($_FILES['FileInput']['name']);
-move_uploaded_file($_FILES['FileInput']['tmp_name'],$FileName);
+
 echo("siker");
 header("Location: https://github.com/Smc05/musicapp");
 exit();
